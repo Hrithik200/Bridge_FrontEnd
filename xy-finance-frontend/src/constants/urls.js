8894a587cbb7d3f97d1a5a299bc42bca039229ae
Hrithik Keswani <hrithikkeswani200@gmail.com>
2024-06-08 18:24:00 +0530

    added backed link

--- xy-finance-frontend/src/constants/urls.js
+++ xy-finance-frontend/src/constants/urls.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5000'; // Backend URL
+export const BASE_URL = 'https://bridge-backend.vercel.app'; // Backend URL
 export const GET_CHAIN=`${BASE_URL}/api/chain`
 export const GET_TOKENS_URL = `${BASE_URL}/api/tokens`;
 export const POST_QUOTES_URL = `${BASE_URL}/api/quotes`;
